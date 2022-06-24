fe6b43fc4f829ef95e97f882a5e082036e46b5e0
Pablo Araújo <rafanet.live@gmail.com>
2022-06-23 22:24:07 -0300

    Configurando JWT

--- src/services/NSE.Identidade.API/Dependencies/IdentityDependencies.cs
+++ src/services/NSE.Identidade.API/Dependencies/IdentityDependencies.cs
@@ -3,17 +3,27 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 using NSE.Identidade.API.Data;
+using NSE.Identidade.API.Extensions;
 
 namespace NSE.Identidade.API.Dependencies;
 
 public static class IdentityDependencies
 {
+    private static readonly IConfiguration _configuration;
+
     public static void IdentityDependency(this IServiceCollection services)
     {
         services.AddDefaultIdentity<IdentityUser>()
             .AddRoles<IdentityRole>()
             .AddEntityFrameworkStores<ApplicationDbContext>()
             .AddDefaultTokenProviders();
+
+        // Jwt
+        var appSettingsSection = _configuration.GetSection("AppSettings");
+        services.Configure<AppSettings>(appSettingsSection);
+
+        var appSettings = appSettingsSection.Get<AppSettings>();
+        var key = Encoding.ASCII.GetBytes(appSettings.Secret);
 
         services.AddAuthentication(options =>
         {
@@ -26,11 +36,11 @@ public static class IdentityDependencies
             bearerOptions.TokenValidationParameters = new TokenValidationParameters
             {
                 ValidateIssuerSigningKey = true,
-                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("x")),
+                IssuerSigningKey = new SymmetricSecurityKey(key),
                 ValidateIssuer = true,
                 ValidateAudience = true,
-                ValidAudience = "x",
-                ValidIssuer = "x"
+                ValidAudience = appSettings.ValidoEm,
+                ValidIssuer = appSettings.Emissor
             };
         });
     }

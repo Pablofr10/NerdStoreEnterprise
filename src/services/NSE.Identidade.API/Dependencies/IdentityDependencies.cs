4e5e80ffae0f8306f50a0fa6c7cf5889dbd27963
Pablo Araújo <rafanet.live@gmail.com>
2022-06-23 22:00:49 -0300

    Adicionando informações do Identity

--- src/services/NSE.Identidade.API/Dependencies/IdentityDependencies.cs
+++ src/services/NSE.Identidade.API/Dependencies/IdentityDependencies.cs
@@ -1,15 +1,38 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 using NSE.Identidade.API.Data;
 
 namespace NSE.Identidade.API.Dependencies;
 
 public static class IdentityDependencies
 {
-    public static void IdentityDependency(this IServiceCollection service)
+    public static void IdentityDependency(this IServiceCollection services)
     {
-        service.AddDefaultIdentity<IdentityUser>()
+        services.AddDefaultIdentity<IdentityUser>()
             .AddRoles<IdentityRole>()
             .AddEntityFrameworkStores<ApplicationDbContext>()
-            .AddDefaultTokenProviders(); }
+            .AddDefaultTokenProviders();
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(bearerOptions =>
+        {
+            bearerOptions.RequireHttpsMetadata = true;
+            bearerOptions.SaveToken = true;
+            bearerOptions.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("x")),
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidAudience = "x",
+                ValidIssuer = "x"
+            };
+        });
+    }
     
 }

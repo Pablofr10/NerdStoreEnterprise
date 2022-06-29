e27571e23e0e9aca9ef7f462abd38eccbf750abc
Pablo Araújo <rafanet.live@gmail.com>
2022-06-28 22:14:49 -0300

    Refatorando configurações

---
+++ src/web/NSE.WebApp.MVC/Configuration/WebAppConfig.cs
@@ -0,0 +1,26 @@
+﻿namespace NSE.WebApp.MVC.Configuration;
+
+public static class WebAppConfig
+{
+    public static void AddMvcConfiguration(this IServiceCollection services)
+    {
+        services.AddControllersWithViews();
+    }
+    public static void UseApiConfiguration(this WebApplication app)
+    {
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+    }
+}
